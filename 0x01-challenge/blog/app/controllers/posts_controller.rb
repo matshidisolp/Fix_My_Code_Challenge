2e1b37a93425b0ea10b495974d2887f31d57488a
matshidisolp <mpitswane@gmail.com>
2024-07-23 08:18:58 +0700

    posts_controller.rb

--- 0x01-challenge/blog/app/controllers/posts_controller.rb
+++ 0x01-challenge/blog/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.where(online: true).order('created_at DESC')
   end
 
   def new
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :online)
     end
 end
